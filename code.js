f45c96f67e425a47d4fa3f00f90cfcd23074a1a6
Jennifer Ho <jenniferho222@gmail.com>
2018-06-29 21:10:55 +0000

    part 2

--- code.js
+++ code.js
@@ -1,9 +1,15 @@
-(".row .footer .1").click(function(){
-    show("#lightbox");
+$(document).ready(function(){
+    
+    $(".top 1").css("color" ,"purple");
+    
+    $(".row .footer .1").click(function(){
+        show("#lightbox");
+    });
 });
+
 function show(selecter){
-          (selecter).css({display: "block"});
-        }
+    $(selecter).css({display: "block"});
+}
 function hide(selecter){
-          (selecter).css({display: "none"});
-        }
+    $(selecter).css({display: "none"});
+}

1c8d3868af3825996e7bd102d1cac9705f0a1bc5
Jennifer Ho <jenniferho222@gmail.com>
2018-06-29 20:01:02 +0000

    hey

---
+++ code.js
@@ -0,0 +1,9 @@
+(".row .footer .1").click(function(){
+    show("#lightbox");
+});
+function show(selecter){
+          (selecter).css({display: "block"});
+        }
+function hide(selecter){
+          (selecter).css({display: "none"});
+        }
